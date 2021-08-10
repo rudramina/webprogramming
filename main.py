ddcfb25fca6a2518898ec4dcfe0cb53170259caa
RudraMina <rudramina@protonmail.com>
2021-08-10 16:28:09 +0530

    deted square.py

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-#variable
-name = "Harry"
-#list
-names = ["harry", "ron", "hermoine"]
-print(names)
-print(f'main - {names[0]}')
-print(name[0])
-# coordinates 
-coordinatex = 10.0
-coordinatey = 20.0
-coordinates = (10.0, 20.0)
-# ---------------
-a= str(input("enter your name: "))
-#appending in list
-names.append(a)
-#----------------
-print(names)
+# #variable
+# name = "Harry"
+# #list
+# names = ["harry", "ron", "hermoine"]
+# print(names)
+# print(f'main - {names[0]}')
+# print(name[0])
+# # coordinates 
+# coordinatex = 10.0
+# coordinatey = 20.0
+# coordinates = (10.0, 20.0)
+# # ---------------
+# a= str(input("enter your name: "))
+# #appending in list
+# names.append(a)
+# #----------------
+# print(names)
+# #sorting
+# names.sort()
+# print(names)
+# # sets
+# s = set()
+# s.add(1)
+# s.add(33)
+# s.add(25)
+# print(s)
+# s.remove(33)
+# print(f"THe set has{len( s )} elements.")
+# for i in names:
+#     print(i)
+# for ch in "Rudra":
+#     print(ch)
+# houses = {"harry":"gryfinndor","draco": "slytherin"}
+# houses["hermoine"] = "gryfindor"
+# print(houses["harry"])
+# print(houses)
+def square(x):
+    return x * x
--- square.py
+++
@@ -1,4 +0,0 @@
-from main import square
-
-for i in range(10):
-    print(f'num - {i} square - {square(i)}')
