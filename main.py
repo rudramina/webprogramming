9bb3f77ba0c1d8b343f8c3da8ae7be3f238e757e
RudraMina <rudramina@protonmail.com>
2021-08-11 08:57:03 +0530

    Objective Orented Programming

--- main.py
+++ main.py
@@ -43,23 +43,25 @@
 # p = Point(12,5)
 # print(p.x)
 # print(p.y)
-class Flight():
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.passengers = []
-    def add_passenger(self,name):
-        if not self.vacant_seats():
-            return False
-        self.passengers.append(name)
-        return True
-    def vacant_seats(self):
-        return self.capacity - len(self.passengers)
+# class Flight():
+#     def __init__(self,capacity):
+#         self.capacity = capacity
+#         self.passengers = []
 
-people = ["harry", "ron", "hermoine", "rudra"]
-flight = Flight(3)
-for person in people:
-    if flight.add_passenger(person):
-        print(f'Added {person} to flight sucessfully')
-    else:
-        print(f'No available seats for {person}')
-    
+#     def add_passenger(self, name):
+#         if not self.vacant_seats():
+#             return False
+#         else:
+#             self.passengers.append(name)
+#             return True
+
+#     def vacant_seats(self):
+#         return self.capacity - len(self.passengers)
+# flight = Flight(3)
+# people = ['harry','hermoine','ron','ginny','draco']
+# for name in people:
+#     success = flight.add_passenger(name)
+#     if success:
+#         print(f"Added {name} successfully")
+#     else:
+#         print(f'No available seats for {name}')
